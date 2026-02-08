0c126644b4e70528c914cf81dd9a10fab2af95c0
King-Jump <github@0xjump.com>
2026-02-09 00:18:49 +0800

    MOD: add test net url

--- octopuspy/exchange/binance_restapi.py
+++ octopuspy/exchange/binance_restapi.py
@@ -53,7 +53,9 @@ class BnSpotClient(BaseClient):
             params: ClientParams,
             logger: Logger=logging.getLogger(__file__)):
         super().__init__(params, logger=logger)
-        self.spot_client = Client(api_key=params.api_key, api_secret=params.secret)
+        # self.spot_client = Client(api_key=params.api_key, api_secret=params.secret,
+        #                           base_url = "https://testnet.binance.vision/api")  # use test_net
+        self.spot_client = Client(api_key=params.api_key, api_secret=params.secret)  # use test_net
 
     def tif_map(self, order_type:str, tif:str):
         if order_type == "LIMIT_MAKER":
